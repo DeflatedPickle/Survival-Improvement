fcd640e62bc61ca7b674b0e9b9715b247910673d
DeflatedPickle <DeflatedPickle@gmail.com>
2018-04-03 02:20:15 +0100

    Added the Reference class

---
+++ src/main/java/com/deflatedpickle/survivalimprovement/Reference.java
@@ -0,0 +1,9 @@
+package com.deflatedpickle.survivalimprovement;
+
+public class Reference {
+    public static final String MOD_ID = "survivalimprovement";
+    public static final String NAME = "SurvivalImprovement";
+    // Versions follow this format: MCVERSION-MAJORMOD.MAJORAPI.MINOR.PATCH.
+    public static final String VERSION = "1.12.2-1.0.0.0";
+    public static final String ACCEPTED_VERSIONS = "[1.12.1, 1.12.2]";
+}
