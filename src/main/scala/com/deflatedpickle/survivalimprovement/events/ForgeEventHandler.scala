bf11dec7fb5d27b34bffb699a0ae54c500cdccc2
DeflatedPickle <DeflatedPickle@gmail.com>
2018-04-03 05:17:12 +0100

    Added an event for melons

--- src/main/scala/com/deflatedpickle/survivalimprovement/events/ForgeEventHandler.scala
+++ src/main/scala/com/deflatedpickle/survivalimprovement/events/ForgeEventHandler.scala
@@ -1,5 +1,20 @@
 package com.deflatedpickle.survivalimprovement.events
 
+import net.minecraft.init.Blocks
+import net.minecraft.item.ItemStack
+import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 class ForgeEventHandler {
+  @SubscribeEvent
+  def onHarvestDropsEvent(event: HarvestDropsEvent): Unit = {
+    if (event.getState.getBlock == Blocks.MELON_BLOCK) {
+      val mainHand = event.getHarvester.getHeldItemMainhand
+
+      if (mainHand.isEmpty || mainHand.getItem.getToolClasses(mainHand).contains("shovel")) {
+        event.getDrops.clear()
+        event.getDrops.add(new ItemStack(Blocks.MELON_BLOCK))
+      }
+    }
+  }
 }
