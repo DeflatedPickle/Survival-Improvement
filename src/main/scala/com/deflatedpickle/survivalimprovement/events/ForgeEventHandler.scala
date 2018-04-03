19070311d2ae6473350c967ed2458797d5653d6a
DeflatedPickle <DeflatedPickle@gmail.com>
2018-04-03 07:51:29 +0100

    Sticks can now drop from leaves

--- src/main/scala/com/deflatedpickle/survivalimprovement/events/ForgeEventHandler.scala
+++ src/main/scala/com/deflatedpickle/survivalimprovement/events/ForgeEventHandler.scala
@@ -1,11 +1,15 @@
 package com.deflatedpickle.survivalimprovement.events
 
-import net.minecraft.init.Blocks
+import java.util.Random
+
+import net.minecraft.init.{Blocks, Items}
 import net.minecraft.item.ItemStack
 import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 class ForgeEventHandler {
+  private val random = new Random()
+
   @SubscribeEvent
   def onHarvestDropsEvent(event: HarvestDropsEvent): Unit = {
     if (event.getState.getBlock == Blocks.MELON_BLOCK) {
@@ -14,6 +18,11 @@ class ForgeEventHandler {
       if (mainHand.isEmpty || mainHand.getItem.getToolClasses(mainHand).contains("shovel")) {
         event.getDrops.clear()
         event.getDrops.add(new ItemStack(Blocks.MELON_BLOCK))
+      }
+    }
+    else if (event.getState.getBlock == Blocks.LEAVES) {
+      if (random.nextInt(4) == 1) {
+        event.getDrops.add(new ItemStack(Items.STICK, random.nextInt(4)))
       }
     }
   }
