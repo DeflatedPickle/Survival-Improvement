e6db6fddb8bce47d3f3bc1de89ed66c2179c01e6
Dibble <DeflatedPickle@gmail.com>
2018-06-14 23:18:12 +0100

    Updated version

--- src/main/kotlin/com/deflatedpickle/survivalimprovement/Reference.kt
+++ src/main/kotlin/com/deflatedpickle/survivalimprovement/Reference.kt
@@ -4,7 +4,7 @@ object Reference {
     const val MOD_ID = "survivalimprovement"
     const val NAME = "SurvivalImprovement"
     // Versions follow this format: MCVERSION-MAJORMOD.MAJORAPI.MINOR.PATCH.
-    const val VERSION = "1.12.2-1.0.3.0"
+    const val VERSION = "1.12.2-2.0.0.0"
     const val ACCEPTED_VERSIONS = "[1.12.1, 1.12.2]"
 
     const val ADAPTER = "net.shadowfacts.forgelin.KotlinAdapter"
