5c726bf00e058b11379edffa002e8a8d08a30452
Dibble <DeflatedPickle@gmail.com>
2018-06-14 16:50:35 +0100

    Updated version

--- src/main/java/com/deflatedpickle/survivalimprovement/Reference.java
+++ src/main/java/com/deflatedpickle/survivalimprovement/Reference.java
@@ -4,6 +4,6 @@ public class Reference {
     public static final String MOD_ID = "survivalimprovement";
     public static final String NAME = "SurvivalImprovement";
     // Versions follow this format: MCVERSION-MAJORMOD.MAJORAPI.MINOR.PATCH.
-    public static final String VERSION = "1.12.2-1.0.1.0";
+    public static final String VERSION = "1.12.2-1.0.2.0";
     public static final String ACCEPTED_VERSIONS = "[1.12.1, 1.12.2]";
 }
