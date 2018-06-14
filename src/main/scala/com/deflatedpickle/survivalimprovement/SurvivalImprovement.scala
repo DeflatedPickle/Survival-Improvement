b1d4e2a24e15c5c08a09dc4346db52a0058c24dd
Dibble <DeflatedPickle@gmail.com>
2018-06-14 21:22:30 +0100

    Smelting saplings now gives dead bushes

--- src/main/scala/com/deflatedpickle/survivalimprovement/SurvivalImprovement.scala
+++ src/main/scala/com/deflatedpickle/survivalimprovement/SurvivalImprovement.scala
@@ -1,10 +1,13 @@
 package com.deflatedpickle.survivalimprovement
 
 import com.deflatedpickle.survivalimprovement.events.ForgeEventHandler
+import net.minecraft.init.Blocks
+import net.minecraft.item.ItemStack
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.Mod.EventHandler
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
+import net.minecraftforge.fml.common.registry.GameRegistry
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
@@ -16,6 +19,8 @@ object SurvivalImprovement {
   def init(event: FMLInitializationEvent): Unit = {
     log.info("Starting Init.")
     MinecraftForge.EVENT_BUS.register(new ForgeEventHandler)
+
+    GameRegistry.addSmelting(Blocks.SAPLING, new ItemStack(Blocks.DEADBUSH), 0)
     log.info("Finished Init.")
   }
 }
