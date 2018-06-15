3d23fe9bce8a795f551b6f0451417f2d11f819d2
Dibble <DeflatedPickle@gmail.com>
2018-06-15 02:04:31 +0100

    Skeletons now drop bone meal if they suffocate

--- src/main/kotlin/com/deflatedpickle/survivalimprovement/events/ForgeEventHandler.kt
+++ src/main/kotlin/com/deflatedpickle/survivalimprovement/events/ForgeEventHandler.kt
@@ -1,8 +1,11 @@
 package com.deflatedpickle.survivalimprovement.events
 
+import net.minecraft.entity.item.EntityItem
+import net.minecraft.entity.monster.EntitySkeleton
 import net.minecraft.init.Blocks
 import net.minecraft.init.Items
 import net.minecraft.item.ItemStack
+import net.minecraftforge.event.entity.living.LivingDropsEvent
 import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import java.util.*
@@ -22,6 +25,27 @@ class ForgeEventHandler {
         } else if (event.state.block == Blocks.LEAVES) {
             if (random.nextInt(4) == 1) {
                 event.drops.add(ItemStack(Items.STICK, random.nextInt(4)))
+            }
+        }
+    }
+
+    @SubscribeEvent
+    fun onLivingDropsEvent(event: LivingDropsEvent) {
+        if (event.source.damageType == "inWall") {
+            if (event.entity is EntitySkeleton) {
+                for (i in event.drops) {
+                    if (i.item.unlocalizedName == "item.bone") {
+                        var count = 0
+                        event.drops.remove(i)
+
+                        for (n in 0 until i.item.count) {
+                            val num = random.nextInt(4)
+                            count += num
+                        }
+
+                        event.drops.add(EntityItem(event.entity.world, event.entity.posX, event.entity.posY, event.entity.posZ, ItemStack(Items.DYE, count, 15)))
+                    }
+                }
             }
         }
     }
